327e1cc99a40edb775ec2fb941426ee26af2e2de
Kirti Gautam <gautamkirti620@gmail.com>
2026-09-24 12:13:37 +0530

    testing cleaned

--- app.py
+++ app.py
@@ -7,13 +7,3 @@ def calculate_total(price, tax):
 def divide(a, b):
     b=0
     return a / b
-
-
-def get_discount(price):
-    return price * 0.10
-
-def is_even(n):
-    if n%2==0:
-        print("odd")
-    else:
-        print("even")
